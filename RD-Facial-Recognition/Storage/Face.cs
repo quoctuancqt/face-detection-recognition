5e95699aca04bc5792fef62871439ee50f0b53c4
tcao <tuan.cao.ss@synova-solutions.io>
2019-08-27 14:54:42 +0700

    Update Solution

---
+++ RD-Facial-Recognition/Storage/Face.cs
@@ -0,0 +1,10 @@
+﻿namespace RD_Facial_Recognition.Storage
+{
+    public class Face
+    {
+        public byte[] Image { get; set; }
+        //public int Id { get; set; }
+        public string Label { get; set; }
+        public int UserId { get; set; }
+    }
+}
